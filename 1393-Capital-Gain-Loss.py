c560acbdf2003c78b28a63b20f509951e4176b22
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2024-11-11 20:50:05 +0300

    1393

---
+++ 1393-Capital-Gain-Loss.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def capital_gainloss(stocks: pd.DataFrame) -> pd.DataFrame:
+    stocks["state"] = stocks["operation"].map({"Buy": -1, "Sell": 1})
+    stocks["price"] = (stocks["state"] * stocks["price"]).astype(float)
+    result = stocks.groupby("stock_name", as_index=False)['price'].sum()
+    result.price = result.price.astype(int)
+    result.columns = ["stock_name", "capital_gain_loss"]
+    return result
