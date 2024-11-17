3f529f418f32a4821139bc9f515d9c1d424941da
Alper Karaca <thisiseternity12@gmail.com>
2024-11-17 16:14:40 +0300

    2900

---
+++ 2900-Longest-Unequal-Adjacent-Groups-Subsequence-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        result = [words[0]]
+        prev = groups[0]
+
+        for i in range(1, len(words)):
+            if groups[i] != prev:
+                result.append(words[i])
+
+            prev = groups[i]
+
+        return result
+    
+###
+
+class Solution2:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        result = [words[0]]
+        for i in range(1, len(words)):
+            if groups[i] != groups[i - 1]:
+                result.append(words[i])
+        
+        return result
