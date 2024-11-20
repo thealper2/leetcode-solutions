7f7121996e61ddd7e1dfe7d6746f6fb26547f931
Alper Karaca <thisiseternity12@gmail.com>
2024-11-20 13:38:38 +0300

    0409

---
+++ 0409-Longest-Palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        d = {}
+        result = 0
+
+        for char in s:
+            if char not in d:
+                d[char] = 1
+            else:
+                d[char] += 1
+
+            if d[char] % 2 == 0:
+                result += 2
+            
+        for count in d.values():
+            if count % 2 == 1:
+                result += 1
+                break
+
+        return result
