9ad84e5b2908b825c3331338d95f42e03fad235f
Alper Karaca <thisiseternity12@gmail.com>
2024-11-25 14:01:44 +0300

    202

---
+++ 0202-Happy-Number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        result = 0
+        seen = set()
+
+        while n != 1:
+            result = 0
+            while n > 0:
+                digit = n % 10
+                result += digit * digit
+                n //= 10
+            n = result
+            if n in seen:
+                return False
+            
+            seen.add(n)
+
+        return True
