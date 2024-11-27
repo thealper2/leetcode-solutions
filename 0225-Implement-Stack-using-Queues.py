ee0a127d11d8e281e02eca204f2c56330bed5a8f
Alper Karaca <thisiseternity12@gmail.com>
2024-11-28 00:09:27 +0300

    225

---
+++ 0225-Implement-Stack-using-Queues.py
@@ -0,0 +1,29 @@
+class MyStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        if not self.empty():
+            popped_value = self.stack.pop()
+            return popped_value
+
+    def top(self) -> int:
+        if not self.empty():
+            return self.stack[-1]
+        else:
+            return []
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
