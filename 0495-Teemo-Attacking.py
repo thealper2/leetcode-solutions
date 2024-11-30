b01aeab1c93408bbff5cd8387f0bef05e1a7b07f
Alper Karaca <thisiseternity12@gmail.com>
2024-11-30 22:06:25 +0300

    495

---
+++ 0495-Teemo-Attacking.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        total = duration
+        for i in range(1, len(timeSeries)):
+            last = timeSeries[i] - timeSeries[i - 1]
+            if duration < last:
+                total += duration
+            else:
+                total += last
+        
+        return total
