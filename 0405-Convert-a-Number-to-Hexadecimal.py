2d1b3d69eec32c089ebdd6818d379e999f607f3c
Alper Karaca <thisiseternity12@gmail.com>
2024-11-30 20:34:41 +0300

    405

---
+++ 0405-Convert-a-Number-to-Hexadecimal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def toHex(self, num: int) -> str:
+        hex_digits = "0123456789abcdef"
+        result = ""
+
+        if num == 0:
+            return "0"
+        
+        if num < 0:
+            num += 1 << 32
+
+        while num >= 1:
+            result = hex_digits[num % 16] + result
+            num //= 16
+
+        return result
