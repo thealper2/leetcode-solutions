193f728bc8b7a6bcc476f31dfe8bf5bd37c8f4b2
Alper Karaca <thisiseternity12@gmail.com>
2025-03-25 23:02:03 +0300

    1920

---
+++ 1920-Build-Array-from-Permutation.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        result = [0] * len(nums)
+
+        for i in range(len(nums)):
+            result[i] = nums[nums[i]]
+
+        return result
