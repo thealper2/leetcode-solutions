9e1c3450606cc9172c053db55ac0868e2a0ed5ab
Alper Karaca <thisiseternity12@gmail.com>
2025-04-01 13:18:44 +0300

    1389

---
+++ 1300-1399/1389-Create-Target-Array-in-the-Given-Order.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        result = []
+        for i in range(len(nums)):
+            result.insert(index[i], nums[i])
+
+        return result
