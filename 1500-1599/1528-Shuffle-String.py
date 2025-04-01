74f4c387269465b0993a7b218c99397430fb76c6
Alper Karaca <thisiseternity12@gmail.com>
2025-04-01 22:58:40 +0300

    1528

---
+++ 1500-1599/1528-Shuffle-String.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        result = [""] * len(s)
+        for i in range(len(s)):
+            result[indices[i]] = s[i]
+
+        return ''.join(result)
