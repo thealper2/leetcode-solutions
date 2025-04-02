e654fa8c37a1969b6b6c861c37aaba7226441444
Alper Karaca <thisiseternity12@gmail.com>
2025-04-02 20:06:02 +0300

    2108

---
+++ 2100-2199/2108-Find-First-Palindromic-String-in-the-Array.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for word in words:
+            if word == word[::-1]:
+                return word
+
+        return ""
