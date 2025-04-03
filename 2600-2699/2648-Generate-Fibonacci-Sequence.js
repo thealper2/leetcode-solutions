587fa88f0f5d28040fa7b9669eff7a9d29916daf
Alper Karaca <thisiseternity12@gmail.com>
2025-04-03 11:33:19 +0300

    2648

---
+++ 2600-2699/2648-Generate-Fibonacci-Sequence.js
@@ -0,0 +1,22 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let a = 0, b = 1;
+    
+    yield a;
+    yield b;
+    
+    while (true) {
+        const next = a + b;
+        yield next;
+        a = b;
+        b = next;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
