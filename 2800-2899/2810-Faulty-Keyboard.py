9310182e716ae45bf67530bcc7bac62bc1961e0d
Alper Karaca <thisiseternity12@gmail.com>
2025-04-03 14:15:24 +0300

    2810

---
+++ 2800-2899/2810-Faulty-Keyboard.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        result = ""
+        for c in s:
+            if c == 'i':
+                result = result[::-1]
+            else:
+                result += c
+
+        return result
