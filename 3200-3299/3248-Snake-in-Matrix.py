c9144eed834220461b9eb4d7907ebdadc05c0209
Alper Karaca <thisiseternity12@gmail.com>
2025-04-04 13:04:56 +0300

    3248

---
+++ 3200-3299/3248-Snake-in-Matrix.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
+        i, j = 0, 0
+        for cmd in commands:
+            if cmd == "UP":
+                i -= 1
+            elif cmd == "RIGHT":
+                j += 1
+            elif cmd == "DOWN":
+                i += 1
+            elif cmd == "LEFT":
+                j -= 1
+        return i * n + j
