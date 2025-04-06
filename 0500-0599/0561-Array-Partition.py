0e49978d1bffdab50b9ce761f53809018322f697
Alper Karaca <thisiseternity12@gmail.com>
2025-04-06 16:34:28 +0300

    561

--- 0500-0599/0561-Array-Partition.py
+++ 0500-0599/0561-Array-Partition.py
@@ -3,14 +3,5 @@ from typing import List
 
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        n = len(nums)
-        for i in range(n):
-            for j in range(0, n-i-1):
-                if nums[j] > nums[j+1]:
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-        
-        total = 0
-        for i in range(0, n, 2):
-            total += nums[i]
-        
-        return total
+        nums.sort()
+        return sum(nums[::2])
