67ee3ed424573dda4f5903947f6329bc7aa20728
Alper Karaca <thisiseternity12@gmail.com>
2025-04-08 23:27:11 +0300

    2960

---
+++ 2900-2999/2960-Count-Tested-Devices-After-Test-Operations.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def countTestedDevices(self, batteryPercentages: List[int]) -> int:
+        count = 0
+        n = len(batteryPercentages)
+
+        for i in range(n):
+            if batteryPercentages[i] > 0:
+                count += 1
+                for j in range(i + 1, n):
+                    batteryPercentages[j] = max(0, batteryPercentages[j] - 1)
+
+        return count
