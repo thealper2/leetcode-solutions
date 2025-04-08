21bb027ac2a9005f603426732be6c4a7b39aa820
Alper Karaca <thisiseternity12@gmail.com>
2025-04-08 20:28:27 +0300

    3168

--- 3100-3199/3168-Minimum-Number-of-Chairs-in-a-Waiting-Room.py
+++ 3100-3199/3168-Minimum-Number-of-Chairs-in-a-Waiting-Room.py
@@ -6,10 +6,10 @@ class Solution:
         for event in s:
             if event == 'E':
                 current_people += 1
-                if current_people > max_people:
-                    max_people = current_people
             
             elif event == 'L':
                 current_people -= 1
+
+            max_people = max(max_people, current_people)
 
         return max_people
