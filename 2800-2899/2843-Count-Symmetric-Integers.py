a513b860425ee726df738d6163b04e09407cb502
Alper Karaca <thisiseternity12@gmail.com>
2025-04-11 23:23:23 +0300

    2843

--- 2800-2899/2843-Count-Symmetric-Integers.py
+++ 2800-2899/2843-Count-Symmetric-Integers.py
@@ -1,22 +1,18 @@
-from typing import List
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        count = 0
+        for num in range(low, high + 1):
+            num_str = str(num)
+            length = len(num_str)
+            if length % 2 != 0:
+                continue
 
-
-def countSymmetricIntegers(low: int, high: int) -> int:
-    count = 0
-    for num in range(low, high + 1):
-        num_str = str(num)
-        length = len(num_str)
-        if length % 2 != 0:
-            continue
-        half = length // 2
-        first_half = num_str[:half]
-        second_half = num_str[half:]
-        sum_first = sum(int(digit) for digit in first_half)
-        sum_second = sum(int(digit) for digit in second_half)
-        if sum_first == sum_second:
-            count += 1
-    return count
-
-
-print(countSymmetricIntegers(1, 100))
-print(countSymmetricIntegers(1200, 1230))
+            half = length // 2
+            first_half = num_str[:half]
+            second_half = num_str[half:]
+            sum_first = sum(int(digit) for digit in first_half)
+            sum_second = sum(int(digit) for digit in second_half)
+            if sum_first == sum_second:
+                count += 1
+        
+        return count
