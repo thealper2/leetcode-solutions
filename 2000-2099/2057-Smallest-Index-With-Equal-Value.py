d7030b2dae2a8105d6ba05fe9df99109a44f94fa
Alper Karaca <thisiseternity12@gmail.com>
2025-04-17 21:33:41 +0300

    2057

---
+++ 2000-2099/2057-Smallest-Index-With-Equal-Value.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            if i % 10 == nums[i]:
+                return i
+            
+        return -1
