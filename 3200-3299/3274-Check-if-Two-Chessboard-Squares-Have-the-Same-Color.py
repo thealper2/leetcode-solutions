d660999deab10b3e65b7c6c836f0ec7b519adbc2
Alper Karaca <thisiseternity12@gmail.com>
2025-04-19 21:22:34 +0300

    3274

--- 3200-3299/3274-Check-if-Two-Chessboard-Squares-Have-the-Same-Color.py
+++ 3200-3299/3274-Check-if-Two-Chessboard-Squares-Have-the-Same-Color.py
@@ -1,15 +1,12 @@
-def checkTwoChessboards(coordinate1: str, coordinate2: str) -> bool:
-    x1 = ord(coordinate1[0])
-    y1 = int(coordinate1[1])
-    
-    x2 = ord(coordinate2[0])
-    y2 = int(coordinate2[1])
+class Solution:
+    def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
+        x1 = ord(coordinate1[0])
+        y1 = int(coordinate1[1])
+        
+        x2 = ord(coordinate2[0])
+        y2 = int(coordinate2[1])
 
-    s1 = x1 + y1
-    s2 = x2 + y2
+        s1 = x1 + y1
+        s2 = x2 + y2
 
-    return s1 % 2 == s2 % 2
-
-
-print(checkTwoChessboards(coordinate1 = "a1", coordinate2 = "c3"))
-print(checkTwoChessboards(coordinate1 = "a1", coordinate2 = "h3"))
+        return s1 % 2 == s2 % 2
