b04b95e32122ac33d7385cfe936bef1d41e243bb
Alper Karaca <thisiseternity12@gmail.com>
2025-04-20 21:41:42 +0300

    1114

---
+++ 1100-1199/1114-Print-in-Order.py
@@ -0,0 +1,20 @@
+from threading import Semaphore
+
+
+class Foo:
+    def __init__(self):
+        self.s1 = Semaphore(0)
+        self.s2 = Semaphore(0)
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        printFirst()
+        self.s1.release()
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self.s1.acquire()
+        printSecond()
+        self.s2.release()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        self.s2.acquire()
+        printThird()
