5120add1bfd758755ba50897cf260bb7d039b464
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2025-05-23 15:44:53 +0300

    3516

---
+++ 3500-3599/3516-Find-Closest-Person.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        if abs(z - x) < abs(z - y):
+            return 1
+        elif abs(z - x) > abs(z - y):
+            return 2
+        else:
+            return 0
