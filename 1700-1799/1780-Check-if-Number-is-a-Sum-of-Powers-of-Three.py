c4144fd3b75065f7be188b03db6df2003da6b02b
Alper Karaca <thisiseternity12@gmail.com>
2025-06-11 17:34:35 +0300

    1780

---
+++ 1700-1799/1780-Check-if-Number-is-a-Sum-of-Powers-of-Three.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        while n > 0:
+            remainder = n % 3
+            
+            if remainder == 2:
+                return False
+            
+            n = n // 3
+
+        return True
