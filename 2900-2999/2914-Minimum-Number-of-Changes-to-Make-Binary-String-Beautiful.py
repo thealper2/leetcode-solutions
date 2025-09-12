3a55fedeaf9a7d86a521630e32ac66d65e8c4b26
Alper Karaca <thisiseternity12@gmail.com>
2025-09-12 22:16:53 +0300

    2914

---
+++ 2900-2999/2914-Minimum-Number-of-Changes-to-Make-Binary-String-Beautiful.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minChanges(self, s: str) -> int:
+        n = len(s)
+        changes = 0
+        for i in range(0, n, 2):
+            if s[i] != s[i + 1]:
+                changes += 1
+
+        return changes
