1b7979f7fce3b60d034ae27ceea25b525481c042
Alper Karaca <thisiseternity12@gmail.com>
2025-12-20 13:13:37 +0300

    3191

---
+++ 3100-3199/3191-Minimum-Operations-to-Make-Binary-Array-Elements-Equal-to-One-1.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        operations = 0
+
+        for i in range(n - 2):
+            if nums[i] == 0:
+                operations += 1
+                nums[i] = 1 - nums[i]
+                nums[i + 1] = 1 - nums[i + 1]
+                nums[i + 2] = 1 - nums[i + 2]
+
+        if nums[-2] == 0 or nums[-1] == 0:
+            return -1
+
+        return operations
