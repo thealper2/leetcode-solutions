48dac218f51fa71c612aa6e3adf2b1b9693a7082
Alper Karaca <thisiseternity12@gmail.com>
2026-03-31 00:47:54 +0300

    3884

---
+++ 3800-3899/3884-First-Matching-Character-From-Both-Ends.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstMatchingIndex(self, s: str) -> int:
+        n = len(s)
+        for i in range(n // 2 + 1):
+            if s[i] == s[n - i - 1]:
+                return i
+
+        return -1
