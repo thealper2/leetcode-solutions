a1b96c537f2b0ca2cd7524b4b725ce44c873b23f
Alper Karaca <thisiseternity12@gmail.com>
2026-05-30 13:12:49 +0300

    3818

---
+++ 3800-3899/3818-Minimum-Prefix-Removal-to-Make-Array-Strictly-Increasing.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def minimumPrefixLength(self, nums: List[int]) -> int:
+        for i in range(len(nums) - 1, 0, -1):
+            if nums[i - 1] >= nums[i]:
+                return i
+
+        return 0
